df64ee8ee68d4b48e6943eb124e9572921694c16
mr.Li <545517745@qq.com>
2026-03-28 14:36:14 +0800

    构建创建问题单的api

---
+++ prisma/client.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = new PrismaClient();
+
+type PrismaClientSingleton = typeof prismaClientSingleton;
+
+const globalForPrisma = global as unknown as { prisma: PrismaClientSingleton };
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton;
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
