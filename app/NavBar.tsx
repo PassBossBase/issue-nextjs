392058c5401c54c52e4d1ba5cf65916e7dbf19b5
mr.Li <545517745@qq.com>
2026-03-28 10:17:18 +0800

    导航栏设置链接样式

---
+++ app/NavBar.tsx
@@ -0,0 +1,47 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
+
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      label: "Issues",
+      href: "/issues",
+    },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href={"/"}>
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classNames({
+              "text-zinc-900": currentPath === link.href,
+              "text-zinc-500": currentPath !== link.href,
+              "hover:text-zinc-900 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
