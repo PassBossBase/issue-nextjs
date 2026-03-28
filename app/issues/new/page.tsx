5c9e179cc80dc9b44404174d910124202b1c567a
mr.Li <545517745@qq.com>
2026-03-28 16:24:08 +0800

    提取错误信息组件

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,6 +9,7 @@ import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/vaildationSchema";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -46,11 +47,7 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
-        {errors.title && (
-          <Text color="red" as="p">
-            {errors.title.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors?.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
@@ -58,12 +55,7 @@ const NewIssuePage = () => {
             return <SimpleMDE placeholder="Description" {...field} />;
           }}
         />
-        {errors.description && (
-          <Text color="red" as="p">
-            {errors.description.message}
-          </Text>
-        )}
-
+        <ErrorMessage>{errors?.description?.message}</ErrorMessage>
         <Button>Submit New Issue</Button>
       </form>
     </div>
